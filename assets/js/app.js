f910ebffe3ed51d0f368736b5343accd3a00197c
Danilo Machado <danilomachado3105@gmail.com>
2026-08-05 17:34:11 -0300

    feat: implement task form toggle

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,10 @@
 const newTaskButton = document.querySelector("#new-task-button");
-
-console.log(newTaskButton);
-
 const taskForm = document.querySelector(".task-form");
+const cancelButton = document.querySelector("#cancel-button");
 
 newTaskButton.addEventListener("click", function () {
     taskForm.classList.remove("hidden");
 });
-
-const cancelButton = document.querySelector("cancel-button")
-
-console.log(cancelButton)
 
 cancelButton.addEventListener("click", function () {
     taskForm.classList.add("hidden");
